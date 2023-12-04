ffd0b04f0d0d060212cc1eb5037ea1cae0b663d2
Tiago Rodrigues <tiago.rodriguesfr2@gmail.com>
2023-12-04 10:09:02 -0300

    Projeto Natal finalizado

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
